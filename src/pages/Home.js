162b115c162d9207daf3c6749d113eda88627c69
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2020-12-25 18:33:11 +0300

    added Sidebar for mobile screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+// Import Components
 
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
-            <Navbar />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
         </>
     )
 }
