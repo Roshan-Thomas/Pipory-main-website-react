b86f7d1a7308fa34a8eba935a0c711639b830688
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2021-01-02 09:47:44 +0300

    added What we Offer section in careers page

--- src/pages/Navbar-Pages/careers.js
+++ src/pages/Navbar-Pages/careers.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import { Layout, HeroInfo } from '../../components'
 import { careersHeroObj } from '../../components/HeroInfoSection/Data'
+import { PostContainer, PostTitle, MarkdownStyles } from '../../templates/postTemplate/TemplateElements'
+import WhatWeOffer from '../../JSON/pages.json'
+import styled from 'styled-components'
+
 
 const Careers = () => {
     return (
         <Layout>
             <HeroInfo {...careersHeroObj} />
+            <CareerContainer>
+                <CareerTitle>What We Offer</CareerTitle>
+                <CareerWrapper>
+                    <MarkdownCareerStyle
+                    source={WhatWeOffer[1].content}
+                    escapeHtml={false}
+                    />
+                </CareerWrapper>
+            </CareerContainer>
         </Layout>
     )
 }
 
 export default Careers
+
+
+const CareerContainer = styled(PostContainer)`
+    margin: 0;
+    background: #121212;
+`
+
+const CareerTitle = styled(PostTitle)`
+    text-align: center;
+    color: #fff;
+`
+
+const CareerWrapper = styled.div`
+    margin: 0 150px;
+    padding: 20px 0;
+`
+
+const MarkdownCareerStyle = styled(MarkdownStyles)`
+    color: #fff;
+    ul, ol {
+		font-size: 25px;
+	}
+`
