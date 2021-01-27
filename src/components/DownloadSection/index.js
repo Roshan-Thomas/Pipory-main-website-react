4c914e04ad4d240de2664c253e8435e410e41836
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2021-01-27 15:04:34 +0300

    added bigger icons and added hover states for download button

--- src/components/DownloadSection/index.js
+++ src/components/DownloadSection/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Button} from '../ButtonElement'
 import { IoLogoGooglePlaystore, IoLogoApple } from 'react-icons/io5'
+import {IconContext} from 'react-icons/lib'
 import logo from '../../images/gatsby-icon.png'
 import {
     DownloadContainer,
@@ -20,44 +21,47 @@ const Download = () => {
 
     return (
         <>
-            <DownloadContainer>
-                <DownloadWrapper>
-                    <LogoWrap>
-                        <Logo src={logo} alt="pipory logo" />
-                    </LogoWrap>
-                    <DownloadH2>Available Now</DownloadH2>
-                    <DownloadLinksWrapper>
-                        <Button
-                            to="/download-applestore"
-                            onMouseEnter={onHover}
-                            onMouseLeave={onHover}
-                            primary="true"
-                            fontBig="true"
-                        >
-                           <IoLogoApple /> Download on Appstore
-                        </Button>
-                        <Button
-                            to="/download-playstore"
-                            onMouseEnter={onHover}
-                            onMouseLeave={onHover}
-                            primary="true"
-                            moveLeft="true"
-                            fontBig="true"
-                        >
-                            <IoLogoGooglePlaystore />Download on Playstore 
-                        </Button>
-                        {/* <Button
-                            to="/request"
-                            onMouseEnter={onHover}
-                            onMouseLeave={onHover}
-                            primary="true"
-                            fontBig="true"
-                        >
-                            Request Access
-                        </Button> */}
-                    </DownloadLinksWrapper>
-                </DownloadWrapper>
-            </DownloadContainer>
+            <IconContext.Provider value={{ style: { paddingRight: '10px', fontSize: '40px'} }}>
+                <DownloadContainer>
+                    <DownloadWrapper>
+                        <LogoWrap>
+                            <Logo src={logo} alt="pipory logo" />
+                        </LogoWrap>
+                        <DownloadH2>Available Now</DownloadH2>
+                        <DownloadLinksWrapper>
+                            <Button
+                                to="/download-applestore"
+                                onMouseEnter={onHover}
+                                onMouseLeave={onHover}
+                                primary="true"
+                                fontBig="true"
+                            >
+                            <IoLogoApple /> Download on Appstore
+                            </Button>
+                            <Button
+                                to="/download-playstore"
+                                onMouseEnter={onHover}
+                                onMouseLeave={onHover}
+                                primary="true"
+                                moveLeft="true"
+                                fontBig="true"
+                            >
+                                <IoLogoGooglePlaystore />Download on Playstore 
+                            </Button>
+                            {/* <Button
+                                to="/request"
+                                onMouseEnter={onHover}
+                                onMouseLeave={onHover}
+                                primary="true"
+                                fontBig="true"
+                            >
+                                Request Access
+                            </Button> */}
+                        </DownloadLinksWrapper>
+                    </DownloadWrapper>
+                </DownloadContainer>
+            </IconContext.Provider>
+            
         </>
     )
 }
